3866e516d0634697d09d1053523270550040a1e5
xingyu711 <61847270+xingyu711@users.noreply.github.com>
2020-05-01 21:57:01 -0700

    first commit

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Navigation.css'
+
+const Navigation = ({onRouteChange, isSignedIn}) => {
+		if (isSignedIn) {
+      return (
+        <nav>
+          <p onClick = {() => onRouteChange('signout')} className='f4 link dim white pa2 pointer'>Sign Out</p>
+        </nav>
+      )
+		} else {
+      return (
+        <nav>
+          <p onClick = {() => onRouteChange('signin')} className='f4 link dim white pa2 pointer'>Sign In</p>
+          <p onClick = {() => onRouteChange('register')} className='f4 link dim white pa2 pointer'>Register</p>
+        </nav>
+      );
+    }
+}
+
+export default Navigation;
